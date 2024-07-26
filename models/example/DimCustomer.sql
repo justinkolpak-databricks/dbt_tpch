83a05acb014f60faad1356ef6771c228c67dc455
Justin Kolpak <justin.kolpak@databricks.com>
2024-07-26 15:42:14 -0500

    add dimcustomer and add sources

--- models/example/DimCustomer.sql
+++ models/example/DimCustomer.sql
@@ -1,27 +1,14 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='materialized_view') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+SELECT sc.c_custkey AS sk_customer_id
+    , sc.c_name as full_name
+    , sc.c_address as address_line_1
+    , sc.c_phone as phone_number
+    , sc.c_acctbal as account_balance
+    , sc.c_mktsegment as market_segment
+    , sc.c_comment as comment
+    , sn.n_name as nation_name
+    , sr.r_name as region_name
+FROM {{ source('tpch', 'silver_customer') }} sc
+LEFT JOIN {{ source('tpch', 'silver_nation') }} sn ON sc.c_nationkey = sn.n_nationkey
+LEFT JOIN {{ source('tpch', 'silver_region') }} sr ON sn.n_regionkey = sr.r_regionkey;
